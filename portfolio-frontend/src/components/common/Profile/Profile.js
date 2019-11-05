282220da7d895714df3ac56403ee51285a2314de
LeeSeul <mary567@hanmail.net>
2019-11-05 19:56:28 +0900

    사진,git

--- portfolio-frontend/src/components/common/Profile/Profile.js
+++ portfolio-frontend/src/components/common/Profile/Profile.js
@@ -2,7 +2,8 @@ import React from 'react';
 import styles from './Profile.scss';
 import profile from '../../../styles/image/profile.jpg';
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const cx = classNames.bind(styles);
 
@@ -13,6 +14,32 @@ const Profile = () => (
             src={profile}
             alt="me"
         />
+        <div className={cx('github')}>
+            <a className={cx('github-link')}
+            href="https://github.com/WhiteSeul"
+            rel="noopener noreferrer"
+            target="_blank"
+            >
+                <FontAwesomeIcon className="icon-size" icon={faGithub} />
+                <div className="text-size">github</div>
+            </a>
+            <a className={cx('Instagram-link')}
+            href="https://www.instagram.com/kiki_raccoon/"
+            rel="noopener noreferrer"
+            target="_blank"
+            >
+                <FontAwesomeIcon className="icon-size" icon={faInstagram} />
+                <div className="text-size">Instagram</div>
+            </a>
+        </div>
+        <div className={cx('introduce')}>
+            <div className={cx('in_en')}>
+            Hi, I'm <span className={cx('color-text')}>Lee Seul</span>
+            </div>
+            <div className={cx('in_co')}>
+                <span className={cx('color-text')}>개발자</span> Developer
+            </div>
+        </div>
     </div>
 );
 
