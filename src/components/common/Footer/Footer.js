e189c083b7ff8c7451de1bd3d86cb114bd49f0d2
LeeSeul <mary567@hanmail.net>
2019-11-06 13:08:10 +0900

    Footer 문구 추가

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -7,7 +7,14 @@ const cx = classNames.bind(styles);
 
 const Footer = () => (
     <div className={cx('footer')}>
-        
+        <div className={cx('ali')}>
+            <div>
+                stacks used
+            </div>
+            <div className={cx('font-bold')}>
+                React.js
+            </div>
+        </div>
     </div>
 );
 
