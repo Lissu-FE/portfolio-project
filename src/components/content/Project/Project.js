574e16806e4cd396abf7af257e373c613d63e771
LeeSeul <mary567@hanmail.net>
2019-11-06 13:44:36 +0900

    프로젝트 컴포넌트 구성

--- src/components/content/Project/Project.js
+++ src/components/content/Project/Project.js
@@ -6,7 +6,9 @@ const cx = classNames.bind(styles);
 
 const Project = () => (
     <div className={cx('project')}>
-        
+        <div className={cx('project-title')}>
+            Projects done so far
+        </div>
     </div>
 );
 
