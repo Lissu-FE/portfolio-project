d730f509d2eab0afbfdaf1b55ebce0c6b868624f
LeeSeul <mary567@hanmail.net>
2019-11-06 11:51:34 +0900

    명함

--- portfolio-frontend/src/components/common/Contact/Contact.js
+++ portfolio-frontend/src/components/common/Contact/Contact.js
@@ -8,6 +8,37 @@ const Contact = () => (
         <div className={cx('contact')}>
             Contact me
         </div>
+        <div className={cx('card')}>
+            <div className={cx('card-content')}>
+                <div className={cx('card-content1')}>
+                    <div className={cx('details')}>
+                        Details
+                    </div>
+                    <div className={cx('email')}>
+                        <span className={cx('content')}>
+                        email
+                        </span>
+                        mary567@hanmail.net
+                    </div>
+                    <div className={cx('email')}>
+                        <span className={cx('content')}>
+                            mobile
+                        </span>
+                        010-7480-0682
+                    </div>
+                </div>
+                <div className={cx('send-mail')}>
+                    <a
+                    className={cx('send-mail-content')}
+                    href="mailto:mary567@hanmail.net"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    >
+                        Send mail
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
 );
 
