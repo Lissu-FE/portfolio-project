93282a706fa5894c915f694c954cabf773c187a9
Lissu-FE <lissu@kakao.com>
2021-04-20 20:06:48 +0900

    change header

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import styles from './Header.scss';
-import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
+import React from "react";
+import styles from "./Header.scss";
+import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
 
 const cx = classNames.bind(styles);
 
 const Header = () => (
-    <nav className={cx('header')}>
-        <Link 
-        className={cx('project')} 
-        activeClass="active"
-        to="/"
-        spy={true}
-        smooth={true}
-        offset={-70}
-        duration={500}>
-            Lee Seul
-        </Link>
-</nav>
+  <nav className={cx("header")}>
+    <Link
+      className={cx("project")}
+      activeClass="active"
+      to="/"
+      spy={true}
+      smooth={true}
+      offset={-70}
+      duration={500}
+    >
+      <span>&lt;</span>
+      Lissu
+      <span>/&gt;</span>
+    </Link>
+  </nav>
 );
 
 export default Header;
--- src/components/common/PageTemplate/PageTemplate.js
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import styles from './PageTemplate.scss';
-import classNames from 'classnames/bind';
-import Header from 'components/common/Header';
-import Footer from 'components/common/Footer';
+import React from "react";
+import styles from "./PageTemplate.scss";
+import classNames from "classnames/bind";
+import Header from "components/common/Header";
+import Footer from "components/common/Footer";
 
 const cx = classNames.bind(styles);
 
-const PageTemplate = ({children}) => (
-    <div className={cx('page-template')}>
-        <Header />
-        {children}
-        <Footer />
-    </div>
+const PageTemplate = ({ children }) => (
+  <div className={cx("pageTemplate")}>
+    <Header />
+    {children}
+    <Footer />
+  </div>
 );
 
 export default PageTemplate;
